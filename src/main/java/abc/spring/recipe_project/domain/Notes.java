5627640f17f8e9bd13a8cc36d173212ef204705d
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-17 16:51:28 +0200

    create entities Recipe and Notes and made the onetoone relations between them

---
+++ src/main/java/abc/spring/recipe_project/domain/Notes.java
@@ -0,0 +1,41 @@
+package abc.spring.recipe_project.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNoted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNoted() {
+        return recipeNoted;
+    }
+
+    public void setRecipeNoted(String recipeNoted) {
+        this.recipeNoted = recipeNoted;
+    }
+}
